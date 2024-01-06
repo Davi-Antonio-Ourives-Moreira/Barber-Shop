3a97dd0c7af5fbe03d58656dc8cde1a130f1264a
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2024-01-06 11:55:22 -0300

    erros

---
+++ Error/__init__.py
@@ -0,0 +1,8 @@
+from .erros import (Todos_Campos_Nao_Preencidos, 
+                    Campo_Nome_Nao_Preencido, 
+                    Campo_Email_Nao_Preencido, 
+                    Campo_Mensagem_Nao_Preencido, 
+                    Campo_Nome_Email_Nao_Preencido, 
+                    Campo_Email_Mensagem_Nao_Preencido, 
+                    Campo_Nome_Mensagem_Nao_Preencido
+                    )
---
+++ Error/erros.py
@@ -0,0 +1,20 @@
+class Todos_Campos_Nao_Preencidos(Exception):
+    pass
+
+class Campo_Nome_Nao_Preencido(Exception):
+    pass
+
+class Campo_Email_Nao_Preencido(Exception):
+    pass
+
+class Campo_Mensagem_Nao_Preencido(Exception):
+    pass
+
+class Campo_Nome_Email_Nao_Preencido(Exception):
+    pass
+
+class Campo_Email_Mensagem_Nao_Preencido(Exception):
+    pass
+
+class Campo_Nome_Mensagem_Nao_Preencido(Exception):
+    pass
