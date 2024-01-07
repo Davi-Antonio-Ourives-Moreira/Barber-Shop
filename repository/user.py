2a437daa649b8c50702d19edaa36d32b5e0b60e2
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2024-01-07 19:49:00 -0300

    banco servidor

--- repository/user.py
+++ repository/user.py
@@ -2,10 +2,10 @@ import psycopg2 as ps
 
 class Banco_Feeds(object):
     def __init__(self) -> None:
-        self.user = "postgres"
-        self.password = "postgres"
-        self.host = "localhost"
-        self.dbname = "Barbearia"
+        self.user = "wqrijdny"
+        self.password = "sprVVa1kI7ot2RHr2OxICBW0xYDFJxDO"
+        self.host = "mahmud.db.elephantsql.com"
+        self.dbname = "wqrijdny"
         self.port = 5432
 
         self.conectar = ps.connect(user=self.user,
@@ -15,6 +15,8 @@ class Banco_Feeds(object):
                                    port=self.port)
         
         self.cursor = self.conectar.cursor()
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS feedbacks(nome_pessoa_feed text, email_pessoa_feed text, mensagem_pessoa_feed text)")
 
     def Adicionar_Feed(self, nome_pessoa, email_pessoa, mensagem_pessoa):
         self.cursor.execute("INSERT INTO feedbacks(nome_pessoa_feed, email_pessoa_feed, mensagem_pessoa_feed) VALUES(%s,%s,%s)", (nome_pessoa, email_pessoa, mensagem_pessoa))
@@ -27,3 +29,5 @@ class Banco_Feeds(object):
         feeds = list(self.cursor.fetchall())
 
         return feeds
+    
+Banco_Feeds()
